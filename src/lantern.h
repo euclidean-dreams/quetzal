4ccd4003ff87b7b907f233f6b3e9b12e32e920b3
josiah <josiah.chapman@gmail.com>
2026-04-27 23:06:33 -0700

    (~)

---
+++ src/lantern.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "paradigm.h"
+#include "pico/stdlib.h"
+#include "generated/ws2812.pio.h"
+
+
+namespace quetzal {
+class Lantern : public Name {
+public:
+    Lattice lattice;
+
+    Lantern() :
+        lattice{RENDER_WIDTH, RENDER_HEIGHT, Pith{{0, 0, 0}}, false} {
+        auto pio = pio0;
+        int sm = 0;
+        uint offset = pio_add_program(pio, &ws2812_program);
+        ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, WS2812_HAS_W);
+        for (int i = 0; i < LED_COUNT; i++) {
+            put_pixel(serialize_color(Color{0, 0, 0}));
+        }
+        sleep_ms(10);
+    }
+
+    static void show_test_pattern() {
+        for (int i = 0; i < LED_COUNT; i++) {
+            auto size = 10;
+            auto start_index = i - size / 2;
+            auto end_index = i + size / 2;
+
+            for (int j = 0; j < start_index; j++) {
+                put_pixel(serialize_color(Color{0, 0, 0}));
+            }
+            for (int j = start_index; j < end_index && j < LED_COUNT; j++) {
+                put_pixel(serialize_color(Color{0, 33, 77}));
+            }
+            for (int j = end_index; j < LED_COUNT; j++) {
+                put_pixel(serialize_color(Color{0, 0, 0}));
+            }
+            sleep_ms(10);
+        }
+        for (int i = LED_COUNT; i > 0; i--) {
+            auto size = 10;
+            auto start_index = i - size / 2;
+            auto end_index = i + size / 2;
+
+            for (int j = 0; j < start_index; j++) {
+                put_pixel(serialize_color(Color{0, 0, 0}));
+            }
+            for (int j = start_index; j < end_index && j < LED_COUNT; j++) {
+                put_pixel(serialize_color(Color{0, 33, 77}));
+            }
+            for (int j = end_index; j < LED_COUNT; j++) {
+                put_pixel(serialize_color(Color{0, 0, 0}));
+            }
+            sleep_ms(10);
+        }
+    }
+
+    static uint32_t serialize_color(Color color) {
+        return (scast<uint32_t>(color.green) << 16) | (scast<uint32_t>(color.red) << 8) | scast<uint32_t>(color.blue);
+    }
+
+    static inline void put_pixel(uint32_t pixel_grb) {
+        pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+    }
+
+    void show() {
+        for (int x = RENDER_WIDTH - 1; x >= 0; x--) {
+            if (x % 2 != 0) {
+                for (int y = 0; y < RENDER_HEIGHT; y++) {
+                    auto pith = lattice.get_pith(x, y);
+                    auto serialized_color = serialize_color(pith.color);
+                    put_pixel(serialized_color);
+                }
+            } else {
+                for (int y = RENDER_HEIGHT - 1; y >= 0; y--) {
+                    auto pith = lattice.get_pith(x, y);
+                    auto serialized_color = serialize_color(pith.color);
+                    put_pixel(serialized_color);
+                }
+            }
+        }
+    }
+};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,13 @@
+#include "paradigm.h"
 #include <iostream>
-#include <vector>
+
+#include "cosmology.h"
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/spi.h"
 #include "hardware/pio.h"
-#include "hardware/clocks.h"
-#include "generated/ws2812.pio.h"
 #include "DmxOutput.h"
-
-// definitions
-/////////////////
-#define RENDER_WIDTH 9
-#define RENDER_HEIGHT 1
-
-#define SPI_DEVICE spi0
-#define SPI_MOSI_PIN 16
-#define SPI_CHIP_SELECT_PIN 17
-#define SPI_CLOCK_PIN 18
-#define SPI_MISO_PIN 19
-
-#define HEADER_SIZE 8
-#define LED_COUNT (RENDER_WIDTH * RENDER_HEIGHT)
-#define BAUDRATE (8 * 1000 * 1000)
-
-#define WS2812_HAS_W false
-#define WS2812_PIN 2
-
+#include "lantern.h"
 
 // dmx
 /////////
@@ -46,6 +28,7 @@ uint8_t previous_header_index = -1;
 uint8_t transmit_buffer[SPI_PACKET_SIZE];
 uint8_t receive_buffer[SPI_PACKET_SIZE];
 
+namespace quetzal {
 static void initialize_spi() {
     spi_init(SPI_DEVICE, BAUDRATE);
     spi_set_slave(SPI_DEVICE, true);
@@ -78,106 +61,13 @@ static bool spi_header_is_valid() {
     }
 }
 
-
-// primitive
-///////////////
-class RGBColor {
-public:
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-
-    RGBColor(uint8_t red, uint8_t green, uint8_t blue) : red{red}, green{green}, blue{blue} {
-    }
-
-    uint32_t serialize() {
-        return ((uint32_t) (green) << 16) | ((uint32_t) (red) << 8) | (uint32_t) (blue);
-    }
-};
-
-
-// ws2812
-////////////
-std::vector<std::vector<RGBColor>> lattice;
-
-static inline void put_pixel(uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
-}
-
-static void initialize_ws2812() {
-    PIO pio = pio0;
-    int sm = 0;
-    uint offset = pio_add_program(pio, &ws2812_program);
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, WS2812_HAS_W);
-
-    lattice = {};
-    lattice.resize(RENDER_WIDTH);
-    for (auto &row: lattice) {
-        row.resize(RENDER_HEIGHT, {0, 0, 0});
-    };
-    std::cout << "ws2812 ready" << std::endl;
-
-    for (int i = 0; i < LED_COUNT; i++) {
-        put_pixel(RGBColor(0, 0, 0).serialize());
-    }
-    sleep_ms(10);
-    for (int i = 0; i < LED_COUNT; i++) {
-        auto size = 10;
-        auto start_index = i - size / 2;
-        auto end_index = i + size / 2;
-
-        for (int j = 0; j < start_index; j++) {
-            put_pixel(RGBColor(0, 0, 0).serialize());
-        }
-        for (int j = start_index; j < end_index && j < LED_COUNT; j++) {
-            put_pixel(RGBColor(0, 33, 77).serialize());
-        }
-        for (int j = end_index; j < LED_COUNT; j++) {
-            put_pixel(RGBColor(0, 0, 0).serialize());
-        }
-        sleep_ms(10);
-    }
-    for (int i = LED_COUNT; i > 0; i--) {
-        auto size = 10;
-        auto start_index = i - size / 2;
-        auto end_index = i + size / 2;
-
-        for (int j = 0; j < start_index; j++) {
-            put_pixel(RGBColor(0, 0, 0).serialize());
-        }
-        for (int j = start_index; j < end_index && j < LED_COUNT; j++) {
-            put_pixel(RGBColor(0, 33, 77).serialize());
-        }
-        for (int j = end_index; j < LED_COUNT; j++) {
-            put_pixel(RGBColor(0, 0, 0).serialize());
-        }
-        sleep_ms(10);
-    }
-    std::cout << "ws2812 showing test pattern" << std::endl;
-}
-
-static void render_lattice() {
-    for (int x = RENDER_WIDTH - 1; x >= 0; x--) {
-        if (x % 2 != 0) {
-            for (int y = 0; y < RENDER_HEIGHT; y++) {
-                auto color = lattice[x][y];
-                auto serialized_color = color.serialize();
-                put_pixel(serialized_color);
-            }
-        } else {
-            for (int y = RENDER_HEIGHT - 1; y >= 0; y--) {
-                auto color = lattice[x][y];
-                auto serialized_color = color.serialize();
-                put_pixel(serialized_color);
-            }
-        }
-    }
-}
-
 // keyhole
 /////////////
 void keyhole_loop() {
-    initialize_ws2812();
+    Lantern lantern{};
+    std::cout << "ws2812 lantern ready" << std::endl;
+    lantern.show_test_pattern();
+    std::cout << "ws2812 lantern showing test pattern" << std::endl;
 
     while (true) {
         spi_write_read_blocking(SPI_DEVICE, transmit_buffer, receive_buffer, SPI_PACKET_SIZE);
@@ -185,17 +75,19 @@ void keyhole_loop() {
             auto x = 0;
             auto y = 0;
             for (int i = HEADER_SIZE; i < SPI_PACKET_SIZE; i += 3) {
-                auto color = RGBColor(receive_buffer[i],
-                                      receive_buffer[i + 1],
-                                      receive_buffer[i + 2]);
-                lattice[x][y] = color;
+                auto color = Color{
+                    receive_buffer[i],
+                    receive_buffer[i + 1],
+                    receive_buffer[i + 2]
+                };
+                lantern.lattice.set_pith(x, y, Pith{color});
                 x++;
                 if (x % RENDER_WIDTH == 0) {
                     x = 0;
                     y++;
                 }
             }
-            render_lattice();
+            lantern.show();
         } else {
             std::cout << "encountered invalid spi header, re-initializing spi..." << std::endl;
             // the spi hardware will happily begin reading halfway through a transmission, as well as other nonsense
@@ -232,11 +124,15 @@ void dmx_loop() {
         }
     }
 }
+}
 
+using namespace quetzal;
 
 // main
 //////////
 int main() {
+    Cosmology cosmology{RENDER_WIDTH, RENDER_HEIGHT, 0, Impressions::workshop};
+
     stdio_init_all();
     std::cout << "(~) quetzal init..." << std::endl;
 
