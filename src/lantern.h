eea47c3bc8af44d237892c46f4141547f192cd7f
josiah <josiah.chapman@gmail.com>
2026-04-27 23:20:29 -0700

    (~)

--- src/lantern.h
+++ src/lantern.h
@@ -9,21 +9,23 @@ namespace quetzal {
 class Lantern : public Name {
 public:
     Lattice lattice;
+    int led_count;
 
-    Lantern() :
-        lattice{RENDER_WIDTH, RENDER_HEIGHT, Pith{{0, 0, 0}}, false} {
+    Lantern(int width, int height)
+        : lattice{width, height, Pith{{0, 0, 0}}, false},
+          led_count{lattice.width * lattice.height} {
         auto pio = pio0;
         int sm = 0;
         uint offset = pio_add_program(pio, &ws2812_program);
         ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, WS2812_HAS_W);
-        for (int i = 0; i < LED_COUNT; i++) {
+        for (int i = 0; i < led_count; i++) {
             put_pixel(serialize_color(Color{0, 0, 0}));
         }
         sleep_ms(10);
     }
 
-    static void show_test_pattern() {
-        for (int i = 0; i < LED_COUNT; i++) {
+    void show_test_pattern() {
+        for (int i = 0; i < led_count; i++) {
             auto size = 10;
             auto start_index = i - size / 2;
             auto end_index = i + size / 2;
@@ -31,15 +33,15 @@ public:
             for (int j = 0; j < start_index; j++) {
                 put_pixel(serialize_color(Color{0, 0, 0}));
             }
-            for (int j = start_index; j < end_index && j < LED_COUNT; j++) {
+            for (int j = start_index; j < end_index && j < led_count; j++) {
                 put_pixel(serialize_color(Color{0, 33, 77}));
             }
-            for (int j = end_index; j < LED_COUNT; j++) {
+            for (int j = end_index; j < led_count; j++) {
                 put_pixel(serialize_color(Color{0, 0, 0}));
             }
             sleep_ms(10);
         }
-        for (int i = LED_COUNT; i > 0; i--) {
+        for (int i = led_count; i > 0; i--) {
             auto size = 10;
             auto start_index = i - size / 2;
             auto end_index = i + size / 2;
@@ -47,10 +49,10 @@ public:
             for (int j = 0; j < start_index; j++) {
                 put_pixel(serialize_color(Color{0, 0, 0}));
             }
-            for (int j = start_index; j < end_index && j < LED_COUNT; j++) {
+            for (int j = start_index; j < end_index && j < led_count; j++) {
                 put_pixel(serialize_color(Color{0, 33, 77}));
             }
-            for (int j = end_index; j < LED_COUNT; j++) {
+            for (int j = end_index; j < led_count; j++) {
                 put_pixel(serialize_color(Color{0, 0, 0}));
             }
             sleep_ms(10);
@@ -66,15 +68,15 @@ public:
     }
 
     void show() {
-        for (int x = RENDER_WIDTH - 1; x >= 0; x--) {
+        for (int x = lattice.width - 1; x >= 0; x--) {
             if (x % 2 != 0) {
-                for (int y = 0; y < RENDER_HEIGHT; y++) {
+                for (int y = 0; y < lattice.height; y++) {
                     auto pith = lattice.get_pith(x, y);
                     auto serialized_color = serialize_color(pith.color);
                     put_pixel(serialized_color);
                 }
             } else {
-                for (int y = RENDER_HEIGHT - 1; y >= 0; y--) {
+                for (int y = lattice.height - 1; y >= 0; y--) {
                     auto pith = lattice.get_pith(x, y);
                     auto serialized_color = serialize_color(pith.color);
                     put_pixel(serialized_color);
